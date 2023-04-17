e26855779ece7e087ce9b6e531448ec26644d564
tahiry-dev <tahirycontact@gmail.com>
2023-04-17 15:55:54 +0300

    first commit

---
+++ src/main/java/com/maintainable/exo/ExoApplication.java
@@ -0,0 +1,13 @@
+package com.maintainable.exo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maintainable/exo/ExoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maintainable.exo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
